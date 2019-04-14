4211415b20c1dce04e22999acfb42eee2110c32b
Kara O <karajo.odle@gmail.com>
2019-04-14 19:47:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz
-  
+  if num % 15 == 0 
+    "FizzBuzz"
+  if num % 5 == 0 
+    "Buzz"
+  if num % 3 == 0
+    "Fizz"
+  if num % 15 !== 0 
+    "nil"
 end
